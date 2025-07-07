de2b682a748e10dd3f1f04fab559c443d7b5013b
jaehunshin <jhshin0422@gmail.com>
2025-07-08 00:02:02 +0900

    Rag - Embed
    - finish writing embed.py code
    - add requirements.txt

--- embed.py
+++ embed.py
@@ -14,9 +14,20 @@ def embed_text(text: str) -> List[float]:
 
     return res.data[0].embedding
 
-
+# 데이터 셋
 text_list = ["오렌지", "설탕 커피", "카푸치노", "coffee"]
 vector_list = [embed_text(text) for text in text_list]
 
 for text, vector in zip(text_list, vector_list):
     print(f"{text} => {len(vector)} 차원 : {vector[:2]}")
+
+# 질문 예시
+question = "커피"
+question_vector = embed_text(question)
+print(f"{question} => {len(question_vector)} 차원 : {question_vector[:2]}")
+
+from sklearn.metrics.pairwise import cosine_similarity
+similarity_list = cosine_similarity([question_vector], vector_list)[0]
+
+for text, similarity in zip(text_list, similarity_list):
+    print(text, similarity)
